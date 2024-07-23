c34b1b671a8a99bccf3dfe15f469a60a9010c0ae
Adam Stolarczyk <adamstolarczyk2000@gmail.com>
2024-07-23 14:42:21 +0200

    gap repair

--- lib/ui/menu_bottom_sheet.dart
+++ lib/ui/menu_bottom_sheet.dart
@@ -18,100 +18,101 @@ class MenuBottomSheet extends StatelessWidget {
           ),
           color: Colors.white,
         ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(32.0, 13.0, 32.0, 32.0),
-          child: Column(
-            children: [
-              Container(
-                width: 47,
-                height: 3,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(1.5)),
-                ),
+        child: Column(
+          children: [
+            const SizedBox(height: 13),
+            Container(
+              width: 47,
+              height: 3,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(1.5)),
               ),
-              const SizedBox(height: 27),
-              MenuBottomItem(
-                text: "O aplikacji Pola",
-                iconPath: 'info',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.aboutPola);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://www.pola-app.pl/m/about");
-                },
-              ),
-              MenuBottomItem(
-                text: "O Klubie Jagielońskim",
-                iconPath: 'info',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.aboutKJ);
-                  Navigator.pushNamed(context, '/web',
-                      arguments:
-                          "https://klubjagiellonski.pl/o-klubie-jagiellonskim/");
-                },
-              ),
-              MenuBottomItem(
-                text: "Jak oceniamy Firmy",
-                iconPath: 'thumbs',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.instructionSet);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://www.pola-app.pl/m/method");
-                },
-              ),
-              MenuBottomItem(
-                text: "Partnerzy",
-                iconPath: 'handshake',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.partners);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://www.pola-app.pl/m/partners");
-                },
-              ),
-              MenuBottomItem(
-                text: "Przyjaciele Poli",
-                iconPath: 'diversity',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.polasFriends);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://www.pola-app.pl/m/friends");
-                },
-              ),
-              MenuBottomItem(
-                text: "Oceń Polę",
-                iconPath: 'star',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.rateUs);
-                  throw UnimplementedError(
-                      "todo when app in store inAppReview.openStoreListing(appStoreId: '...',);");
-                },
-              ),
-              MenuBottomItem(
-                text: "Zespół",
-                iconPath: 'groups',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.team);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://www.pola-app.pl/m/team");
-                },
-              ),
-              MenuBottomItem(
-                text: "Github",
-                iconPath: 'github',
-                onClick: () {
-                  analytics.aboutOpened(AnalyticsAboutRow.github);
-                  Navigator.pushNamed(context, '/web',
-                      arguments: "https://github.com/KlubJagiellonski");
-                },
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: 296,
-                height: 1,
-                color: const Color(0xFFF0F0F0),
-              ),
-              const SizedBox(height: 20),
-              Align(
+            ),
+            const SizedBox(height: 27),
+            MenuBottomItem(
+              text: "O aplikacji Pola",
+              iconPath: 'info',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.aboutPola);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://www.pola-app.pl/m/about");
+              },
+            ),
+            MenuBottomItem(
+              text: "O Klubie Jagielońskim",
+              iconPath: 'info',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.aboutKJ);
+                Navigator.pushNamed(context, '/web',
+                    arguments:
+                        "https://klubjagiellonski.pl/o-klubie-jagiellonskim/");
+              },
+            ),
+            MenuBottomItem(
+              text: "Jak oceniamy Firmy",
+              iconPath: 'thumbs',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.instructionSet);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://www.pola-app.pl/m/method");
+              },
+            ),
+            MenuBottomItem(
+              text: "Partnerzy",
+              iconPath: 'handshake',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.partners);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://www.pola-app.pl/m/partners");
+              },
+            ),
+            MenuBottomItem(
+              text: "Przyjaciele Poli",
+              iconPath: 'diversity',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.polasFriends);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://www.pola-app.pl/m/friends");
+              },
+            ),
+            MenuBottomItem(
+              text: "Oceń Polę",
+              iconPath: 'star',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.rateUs);
+                throw UnimplementedError(
+                    "todo when app in store inAppReview.openStoreListing(appStoreId: '...',);");
+              },
+            ),
+            MenuBottomItem(
+              text: "Zespół",
+              iconPath: 'groups',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.team);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://www.pola-app.pl/m/team");
+              },
+            ),
+            MenuBottomItem(
+              text: "Github",
+              iconPath: 'github',
+              onClick: () {
+                analytics.aboutOpened(AnalyticsAboutRow.github);
+                Navigator.pushNamed(context, '/web',
+                    arguments: "https://github.com/KlubJagiellonski");
+              },
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: 296,
+              height: 1,
+              color: const Color(0xFFF0F0F0),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 32.0),
+              child: Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
                   "Znajdź nas tutaj",
@@ -121,70 +122,68 @@ class MenuBottomSheet extends StatelessWidget {
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
-// Użycie Row i Expanded dla guzików
-              SizedBox(
-                height: 60,
-                child: Row(
-                  mainAxisAlignment:
-                      MainAxisAlignment.start, // Umożliwia przesuwanie w lewo
-                  children: [
-                    Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            HorizontalButton(
-                              text: "Twitter",
-                              onPressed: () {
-                                analytics
-                                    .aboutOpened(AnalyticsAboutRow.twitter);
-                                Navigator.pushNamed(context, '/web',
-                                    arguments: "https://twitter.com/pola_app");
-                              },
-                            ),
-                            HorizontalButton(
-                              text: "Facebook",
-                              onPressed: () {
-                                analytics
-                                    .aboutOpened(AnalyticsAboutRow.facebook);
-                              },
-                            ),
-                            HorizontalButton(
-                              text: "facebook",
-                              onPressed: () {
-                                analytics
-                                    .aboutOpened(AnalyticsAboutRow.facebook);
-                              },
-                            ),
-                            HorizontalButton(
-                              text: "facebook",
-                              onPressed: () {
-                                analytics
-                                    .aboutOpened(AnalyticsAboutRow.facebook);
-                              },
-                            ),
-                          ],
-                        ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          HorizontalButton(
+                            text: "Twitter",
+                            onPressed: () {
+                              analytics.aboutOpened(AnalyticsAboutRow.twitter);
+                              Navigator.pushNamed(context, '/web',
+                                  arguments: "https://twitter.com/pola_app");
+                            },
+                          ),
+                          HorizontalButton(
+                            text: "Facebook",
+                            onPressed: () {
+                              analytics.aboutOpened(AnalyticsAboutRow.facebook);
+                            },
+                          ),
+                          HorizontalButton(
+                            text: "facebook",
+                            onPressed: () {
+                              analytics.aboutOpened(AnalyticsAboutRow.facebook);
+                            },
+                          ),
+                          HorizontalButton(
+                            text: "facebook",
+                            onPressed: () {
+                              analytics.aboutOpened(AnalyticsAboutRow.facebook);
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 32.0),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    "Aplikacja Pola \n©Klub Jagielloński",
+                    "Aplikacja Pola \n© Klub Jagielloński",
                     style: const TextStyle(
                         fontSize: 14, fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
@@ -211,7 +210,7 @@ class MenuBottomItem extends StatelessWidget {
     return GestureDetector(
       onTap: onClick,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        padding: const EdgeInsets.only(top: 16, left: 32, right: 32),
         child: Row(
           children: [
             Image.asset("assets/icons/$iconPath.png"),
