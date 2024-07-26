bb37762a44e93ab22b6c08300eb1080938ac8f2c
Adam Stolarczyk <adamstolarczyk2000@gmail.com>
2024-07-26 13:42:23 +0200

    improved file separation

--- lib/ui/menu_bottom_sheet.dart
+++ lib/ui/menu_bottom_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pola_flutter/analytics/analytics_about_row.dart';
 import 'package:pola_flutter/analytics/pola_analytics.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:pola_flutter/ui/social_media_list_view.dart';
 import 'menu_item_list_view.dart';
 
 class MenuBottomSheet extends StatelessWidget {
@@ -49,86 +48,6 @@ class MenuBottomSheet extends StatelessWidget {
   }
 }
 
-class SocialMediaListview extends StatelessWidget {
-  final PolaAnalytics analytics;
-
-  const SocialMediaListview({super.key, required this.analytics});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            const SizedBox(width: 32.0),
-            const Text(
-              "Znajdź nas tutaj",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Lato',
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 12),
-        SizedBox(
-          height: 60,
-          child: Row(
-            children: [
-              const SizedBox(width: 32.0),
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  clipBehavior: Clip.none,
-                  child: Row(
-                    children: [
-                      HorizontalButton(
-                        text: "Twitter",
-                        onPressed: () {
-                          analytics.aboutOpened(AnalyticsAboutRow.twitter);
-                          _launchURL('https://twitter.com/pola_app');
-                        },
-                      ),
-                      const SizedBox(width: 14.0),
-                      HorizontalButton(
-                        text: "Facebook",
-                        onPressed: () {
-                          analytics.aboutOpened(AnalyticsAboutRow.facebook);
-                          _launchURL('https://facebook.com');
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 17.0),
-        Row(
-          children: [
-            const SizedBox(width: 32.0),
-            const Text(
-              "Aplikacja Pola \n© Klub Jagielloński",
-              style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: 'Lato'),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  void _launchURL(String url) async {
-    launchUrl(
-      Uri.parse(url),
-      mode: LaunchMode.externalApplication,
-    );
-  }
-}
 
 class HorizontalButton extends StatelessWidget {
   final String text;
