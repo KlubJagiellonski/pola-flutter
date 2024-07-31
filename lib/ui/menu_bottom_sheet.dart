c5cd12bd38a36f828380065e8c8415769ee196db
D3bi7 <167427782+D3bi7@users.noreply.github.com>
2024-07-31 08:54:14 +0200

    Redesing ekran menu (#87)
    * add icons in menu bottom sheet
    
    * adding distances from the edges
    
    * change line indicator
    
    * adding lower buttons
    
    * gap repair
    
    * adding url links
    
    * adding spacing
    
    * adding spaces
    
    * add fonts
    
    * add fonts
    
    * fixes review
    
    * file separation
    
    * improved file separation
    
    * bug fixes
    
    * bug fixes
    
    * improved file separation
    
    * deleting unnecessary files
    
    * adding a private class
    
    * navigate to url  in webViewItem
    
    * _externalUrlItem -> _webViewItem

--- lib/ui/menu_bottom_sheet.dart
+++ lib/ui/menu_bottom_sheet.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:pola_flutter/analytics/analytics_about_row.dart';
 import 'package:pola_flutter/analytics/pola_analytics.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:pola_flutter/ui/social_media_list_view.dart';
+import 'menu_item_list_view.dart';
 
 class MenuBottomSheet extends StatelessWidget {
   final PolaAnalytics analytics;
@@ -14,175 +13,37 @@ class MenuBottomSheet extends StatelessWidget {
     return SingleChildScrollView(
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: new BorderRadius.only(
-              topLeft: const Radius.circular(10.0),
-              topRight: const Radius.circular(10.0)),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding:
-              EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0, bottom: 8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                    left: 64.0, top: 8.0, right: 64.0, bottom: 8.0),
-                child: Divider(
-                    height: 1,
-                    thickness: 1,
-                    indent: 64,
-                    endIndent: 64,
-                    color: Colors.black),
-              ),
-              _WebViewItem(
-                "O aplikacji Pola", 
-                analytics, 
-                AnalyticsAboutRow.aboutPola,
-                "https://www.pola-app.pl/m/about"
-              ),
-              _WebViewItem(
-                "Instrukcja obsługi", 
-                analytics, 
-                AnalyticsAboutRow.instructionSet,
-                "https://www.pola-app.pl/m/method"
-              ),
-              _WebViewItem(
-                "O Klubie Jagiellońskim",
-                analytics,
-                AnalyticsAboutRow.aboutKJ,
-                "https://klubjagiellonski.pl/o-klubie-jagiellonskim/"
-              ),
-              _WebViewItem(
-                "Zespół",
-                analytics,
-                AnalyticsAboutRow.team,
-                "https://www.pola-app.pl/m/team"
-              ),
-              _WebViewItem(
-                "Partnerzy",
-                analytics,
-                AnalyticsAboutRow.partners,
-                "https://www.pola-app.pl/m/partners"
-              ),
-              _WebViewItem(
-                "Przyjaciele Poli",
-                analytics,
-                AnalyticsAboutRow.polasFriends,
-                "https://www.pola-app.pl/m/friends"
-              ),
-              _ExternalUrlItem(
-                "GitHub",
-                analytics,
-                AnalyticsAboutRow.github,
-                "https://github.com/KlubJagiellonski/pola-flutter"
-              ),
-              _ExternalUrlItem(
-                "Oceń Polę",
-                analytics,
-                AnalyticsAboutRow.rateUs,
-                Platform.isIOS 
-                  ? "https://apps.apple.com/app/id1038401148"
-                  : "https://play.google.com/store/apps/details?id=pl.pola_app"
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      child: _ExternalUrlItem(
-                          "Facebook",
-                          analytics,
-                          AnalyticsAboutRow.facebook,
-                          "https://www.facebook.com/app.pola"
-                        )
-                  ),
-                  Expanded(
-                      child: _ExternalUrlItem(
-                          "X",
-                          analytics,
-                          AnalyticsAboutRow.twitter,
-                          "https://twitter.com/pola_app"
-                        )
-                 )
-                ],
-              ),
-              Padding(
-                  padding: EdgeInsets.all(6.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Aplikacja Pola \n©Klub Jagielloński"),
-                  ))
-            ],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _ExternalUrlItem extends StatelessWidget {
-  _ExternalUrlItem(this.text, this.analytics, this.analyticsRow, this.url);
-
-  final String text;
-  final PolaAnalytics analytics;
-  final AnalyticsAboutRow analyticsRow;
-  final String url;
-
-  @override
-  Widget build(BuildContext context) {
-    return _MenuBottomItem(text, () {
-      analytics.aboutOpened(analyticsRow);
-      launchUrl(
-        Uri.parse(url),
-        mode: LaunchMode.externalApplication,
-      );
-    });
-  }
-}
-
-class _WebViewItem extends StatelessWidget {
-  _WebViewItem(this.text, this.analytics, this.analyticsRow, this.url);
-
-  final String text;
-  final PolaAnalytics analytics;
-  final AnalyticsAboutRow analyticsRow;
-  final String url;
-
-  @override
-  Widget build(BuildContext context) {
-    return _MenuBottomItem(text, () {
-      analytics.aboutOpened(analyticsRow);
-      Navigator.pushNamed(context, '/web', arguments: url);
-    });
-  }
-}
-
-class _MenuBottomItem extends StatelessWidget {
-  _MenuBottomItem(this.text, this.onClick);
-
-  final String text;
-  final Function onClick;
-
-  final textStyle = TextStyle(fontWeight: FontWeight.w600);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Container(
-        width: double.infinity,
-        decoration: new BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(4.0)),
           color: Colors.white,
-          boxShadow: [
-            BoxShadow(color: Colors.black12, spreadRadius: 1),
-          ],
         ),
-        child: GestureDetector(
-          onTap: () {
-            onClick.call();
-          },
-          child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(text, style: textStyle)),
+        child: Column(
+          children: [
+            const SizedBox(height: 13),
+            Container(
+              width: 47,
+              height: 3,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(1.5)),
+              ),
+            ),
+            const SizedBox(height: 11),
+            MenuItemListView(analytics: analytics),
+            const SizedBox(height: 23),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Container(
+                height: 1,
+                color: const Color(0xFFF0F0F0),
+              ),
+            ),
+            const SizedBox(height: 17),
+            SocialMediaListView(analytics: analytics),
+            const SizedBox(height: 33),
+          ],
         ),
       ),
     );
---
+++ lib/ui/social_media_list_view.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:pola_flutter/analytics/analytics_about_row.dart';
+import 'package:pola_flutter/analytics/pola_analytics.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SocialMediaListView extends StatelessWidget {
+  final PolaAnalytics analytics;
+
+  const SocialMediaListView({super.key, required this.analytics});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: const [
+            SizedBox(width: 32.0),
+            Text(
+              "Znajdź nas tutaj",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Lato',
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 12),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  clipBehavior: Clip.none,
+                  child: Row(
+                    children: [
+                      _socialItem(
+                        text: "Twitter",
+                        analyticsRow: AnalyticsAboutRow.twitter,
+                        url: 'https://twitter.com/pola_app',
+                      ),
+                      const SizedBox(width: 14.0),
+                      _socialItem(
+                        text: "Facebook",
+                        analyticsRow: AnalyticsAboutRow.facebook,
+                        url: 'https://facebook.com',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 17.0),
+        Row(
+          children: const [
+            SizedBox(width: 32.0),
+            Text(
+              "Aplikacja Pola \n© Klub Jagielloński",
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Lato'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _socialItem({
+    required String text,
+    required AnalyticsAboutRow analyticsRow,
+    required String url,
+  }) {
+    return SocialItemView(
+      text: text,
+      onPressed: () {
+        analytics.aboutOpened(analyticsRow);
+        _launchURL(url);
+      },
+    );
+  }
+
+  void _launchURL(String url) async {
+    await launchUrl(
+      Uri.parse(url),
+      mode: LaunchMode.externalApplication,
+    );
+  }
+}
+
+class SocialItemView extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const SocialItemView({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: const Color(0xFFE1203E),
+          backgroundColor: const Color(0xFFF5DEDD),
+          textStyle: const TextStyle(fontFamily: 'Roboto'),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        ),
+        onPressed: onPressed,
+        child: Text(text),
+      ),
+    );
+  }
+}
