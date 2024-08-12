bb552298f2fe9aff7acb72cb3eb6461c3598012b
Adam Stolarczyk <adamstolarczyk2000@gmail.com>
2024-08-12 14:10:17 +0200

    changing png to svg

---
+++ lib/pages/detail/detail_content.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+import 'package:pola_flutter/models/company.dart';
+import 'package:pola_flutter/models/search_result.dart';
+import 'package:pola_flutter/i18n/strings.g.dart';
+import 'package:pola_flutter/theme/fonts.gen.dart';
+import 'logotypes.dart';
+import 'expandandable_text.dart';
+import 'polish_capital_graph.dart';
+import 'package:pola_flutter/theme/assets.gen.dart';
+
+class DetailContent extends StatelessWidget {
+  DetailContent(this.searchResult);
+
+  final SearchResult searchResult;
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchResult.companies == null) {
+      return Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(searchResult.altText ?? ""),
+      );
+    }
+
+    final company = searchResult.companies!.first;
+    final score = company.plScore ?? 0;
+    final double plCapital = (company.plCapital ?? 0).toDouble();
+    final Translations t = Translations.of(context);
+
+    final hasLogo = company.logotypeUrl != null;
+    final hasDescription = company.description?.isNotEmpty ?? false;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 17.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Assets.info.svg(height: 24.0,width: 24.0),
+            
+
+                SizedBox(width: 8.0),
+                Text(
+                  t.companyScreen.ourRating,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: FontFamily.lato,
+                     color: Color(0xFF1C1B1F),
+                  ),
+                ),
+                SizedBox(width: 8.0),
+                Text(
+                  t.companyScreen.points(score: score),
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: FontFamily.lato,
+                     color: Color(0xFF1C1B1F),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: LinearProgressIndicator(
+                value: score / 100.0,
+                backgroundColor: Color(0xFFF5DEDD),
+                valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFE1203E)),
+                minHeight: 12.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 17.0),
+          Divider(
+            thickness: 1.0,
+            color: Color(0xFFF0F0F0),
+            indent: 0,
+            endIndent: 0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 22.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                t.companyScreen.gradingCriteria,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: FontFamily.lato,
+                   color: Color(0xFF1C1B1F),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 22.0),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PolishCapitalGraph(percentage: plCapital),
+              SizedBox(width: 35.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _DetailItem(t.companyScreen.producedInPoland,
+                        (company.plWorkers ?? 0) != 0),
+                    SizedBox(height: 14.0),
+                    _DetailItem(t.companyScreen.researchInPoland,
+                        (company.plRnD ?? 0) != 0),
+                    SizedBox(height: 14.0),
+                    _DetailItem(t.companyScreen.registeredInPoland,
+                        (company.plRegistered ?? 0) != 0),
+                    SizedBox(height: 14.0),
+                    _DetailItem(t.companyScreen.notConcernPart,
+                        (company.plNotGlobEnt ?? 0) != 0),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 22.0),
+          Divider(
+            thickness: 1.0,
+            color: Color(0xFFF0F0F0),
+            indent: 0,
+            endIndent: 0,
+          ),
+          if (hasDescription) ...[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 22.0),
+              child: ExpandableText(company.description ?? ""),
+            ),
+            if (hasLogo)
+              Divider(
+                thickness: 1.0,
+                color: Color(0xFFF0F0F0),
+                indent: 0,
+                endIndent: 0,
+              ),
+          ],
+          if (hasLogo)
+            Logotypes(
+                logotypes: searchResult.logotypes(),
+                searchResult: searchResult),
+          SizedBox(height: 26.0),
+          if (hasLogo)
+            Divider(
+              thickness: 1.0,
+              color: Color(0xFFF0F0F0),
+              indent: 0,
+              endIndent: 0,
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DetailItem extends StatelessWidget {
+  _DetailItem(this.text, this.state);
+
+  final String text;
+  final bool state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: 3.0),
+          child: state ? Assets.taskAlt.svg() : Assets.radioButtonUnchecked.svg()
+        ),
+        Expanded(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.w400,
+              fontFamily: FontFamily.lato,
+               color: Color(0xFF1C1B1F),
+            ),
+            softWrap: true,
+            overflow: TextOverflow.visible,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+extension on SearchResult {
+  List<Logotype> logotypes() {
+    var brandLogotypes = allCompanyBrands?.map((brand) {
+          final brandLogotype = brand.logotypeUrl;
+          if (brandLogotype != null) {
+            return Logotype(brandLogotype, null);
+          } else {
+            return null;
+          }
+        }).toList() ??
+        [];
+
+    final logotypeCompany = companies?.first.logotype();
+
+    brandLogotypes.insert(0, logotypeCompany);
+
+    return brandLogotypes
+        .where((logotype) => logotype != null)
+        .cast<Logotype>()
+        .toList();
+  }
+}
+
+extension on Company {
+  Logotype? logotype() {
+    final logotypeUrl = this.logotypeUrl;
+    if (logotypeUrl != null) {
+      return Logotype(logotypeUrl, officialUrl);
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/theme/assets.gen.dart
@@ -0,0 +1,234 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+
+class $AssetsMenuPageGen {
+  const $AssetsMenuPageGen();
+
+  /// File path: assets/menuPage/diversity_1.svg
+  SvgGenImage get diversity1 => const SvgGenImage('assets/menuPage/diversity_1.svg');
+
+  /// File path: assets/menuPage/github-mark 1.svg
+  SvgGenImage get githubMark1 => const SvgGenImage('assets/menuPage/github-mark 1.svg');
+
+  /// File path: assets/menuPage/groups.svg
+  SvgGenImage get groups => const SvgGenImage('assets/menuPage/groups.svg');
+
+  /// File path: assets/menuPage/handshake.svg
+  SvgGenImage get handshake => const SvgGenImage('assets/menuPage/handshake.svg');
+
+  /// File path: assets/menuPage/info.svg
+  SvgGenImage get info => const SvgGenImage('assets/menuPage/info.svg');
+
+  /// File path: assets/menuPage/star_rate.svg
+  SvgGenImage get starRate => const SvgGenImage('assets/menuPage/star_rate.svg');
+
+  /// File path: assets/menuPage/thumbs_up_down.svg
+  SvgGenImage get thumbsUpDown => const SvgGenImage('assets/menuPage/thumbs_up_down.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [diversity1, githubMark1, groups, handshake, info, starRate, thumbsUpDown];
+}
+
+class Assets {
+  Assets._();
+
+  static const AssetGenImage back = AssetGenImage('assets/back.png');
+  static const AssetGenImage icAddBlack24dp = AssetGenImage('assets/ic_add_black_24dp.png');
+  static const AssetGenImage icBackspaceWhite36dp = AssetGenImage('assets/ic_backspace_white_36dp.png');
+  static const AssetGenImage icDialpadWhite36dp = AssetGenImage('assets/ic_dialpad_white_36dp.png');
+  static const AssetGenImage icDoneWhite36dp = AssetGenImage('assets/ic_done_white_36dp.png');
+  static const AssetGenImage icFlashOffWhite48dp = AssetGenImage('assets/ic_flash_off_white_48dp.png');
+  static const AssetGenImage icFlashOnWhite48dp = AssetGenImage('assets/ic_flash_on_white_48dp.png');
+  static const AssetGenImage icHeart = AssetGenImage('assets/ic_heart.png');
+  static const AssetGenImage icLauncher = AssetGenImage('assets/ic_launcher.png');
+  static const SvgGenImage info = SvgGenImage('assets/info.svg');
+  static const AssetGenImage menu = AssetGenImage('assets/menu.png');
+  static const $AssetsMenuPageGen menuPage = $AssetsMenuPageGen();
+  static const SvgGenImage radioButtonUnchecked = SvgGenImage('assets/radio_button_unchecked.svg');
+  static const SvgGenImage taskAlt = SvgGenImage('assets/task_alt.svg');
+
+  /// List of all assets
+  static List<dynamic> get values => [
+        back,
+        icAddBlack24dp,
+        icBackspaceWhite36dp,
+        icDialpadWhite36dp,
+        icDoneWhite36dp,
+        icFlashOffWhite48dp,
+        icFlashOnWhite48dp,
+        icHeart,
+        icLauncher,
+        info,
+        menu,
+        radioButtonUnchecked,
+        taskAlt
+      ];
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  }) : _isVecFormat = false;
+
+  const SvgGenImage.vec(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  }) : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final BytesLoader loader;
+    if (_isVecFormat) {
+      loader = AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+      );
+    }
+    return SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter: colorFilter ?? (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
