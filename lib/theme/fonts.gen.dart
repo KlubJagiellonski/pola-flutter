ff0cf090003d331889fc91e16d4ff0a6bc0ce3b7
Adam Stolarczyk <adamstolarczyk2000@gmail.com>
2024-08-13 10:00:38 +0200

    add class text size

--- lib/theme/fonts.gen.dart
+++ lib/theme/fonts.gen.dart
@@ -1,7 +1,7 @@
 /// GENERATED CODE - DO NOT MODIFY BY HAND
 /// *****************************************************
 ///  FlutterGen
-/// *****************************************************
+/// ** ***************************************************
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -15,4 +15,23 @@ class FontFamily {
 
   /// Font family: Roboto
   static const String roboto = 'Roboto';
+}
+
+class TextSize {
+  TextSize._();
+
+  /// Text size for Page title
+  static const double pageTitle = 24.0;
+
+  /// Text size for News title
+  static const double newsTitle = 20.0;
+
+  /// Text size for Medium title
+  static const double mediumTitle = 16.0;
+
+  /// Text size for Small title
+  static const double smallTitle = 12.0;
+
+  /// Text size for Description
+  static const double description = 11.0;
 }
