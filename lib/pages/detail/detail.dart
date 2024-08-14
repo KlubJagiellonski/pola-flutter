4b95c1826af0edbe519f9b4c90e4331392a80f8e
Karol Stolarczyk <karolstolarczyk157@gmail.com>
2024-08-14 08:00:18 +0200

    101/ do an animation

--- lib/pages/detail/detail.dart
+++ lib/pages/detail/detail.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:pola_flutter/models/search_result.dart';
 import 'detail_lidl.dart';
 import 'detail_content.dart';
@@ -16,11 +16,10 @@ class DetailPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      
       appBar: AppBar(
-        title: Text(searchResult.name ?? ""),
+        title: _MarqueeText(searchResult.name ?? ""),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
@@ -28,12 +27,80 @@ class DetailPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
               DetailContent(searchResult),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _MarqueeText extends StatefulWidget {
+  final String text;
+
+  const _MarqueeText(this.text, {Key? key}) : super(key: key);
+
+  @override
+  State<_MarqueeText> createState() => _MarqueeTextState();
+}
+
+class _MarqueeTextState extends State<_MarqueeText> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  static const int _maxCharacterCount = 20; 
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 10000),
+      vsync: this,
+    )..repeat();
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+    return Container(
+      width: double.infinity,
+      child: ClipRect(
+        child: widget.text.length > _maxCharacterCount 
+          ? _buildAnimatedText(textStyle) 
+          : Text(widget.text, style: textStyle),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedText(TextStyle textStyle) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Row(
+          children: [
+            Transform.translate(
+              offset: Offset(-MediaQuery.of(context).size.width * _animation.value, 0),
+              child: Row(
+                children: [
+                  Text(widget.text, style: textStyle),
+                  const SizedBox(width: 20),  
+                  Text(widget.text, style: textStyle),  
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
