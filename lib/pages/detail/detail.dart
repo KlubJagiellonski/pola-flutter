e6d58be50a788f75be6dcb18b3081954e36e3825
Karol Stolarczyk <karolstolarczyk157@gmail.com>
2024-08-21 10:45:00 +0200

    checking

--- lib/pages/detail/detail.dart
+++ lib/pages/detail/detail.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:pola_flutter/models/search_result.dart';
 import 'detail_lidl.dart';
 import 'detail_content.dart';
@@ -16,9 +16,14 @@ class DetailPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      
       appBar: AppBar(
-        title: Text(searchResult.name ?? ""),
+        title: Container(
+          height: kToolbarHeight,
+          child: MarqueeText(
+            text: searchResult.name ?? "",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+        ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
@@ -35,5 +40,63 @@ class DetailPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class MarqueeText extends StatefulWidget {
+  final String text;
+  final TextStyle style;
+
+  const MarqueeText({Key? key, required this.text, required this.style}) : super(key: key);
+
+  @override
+  _MarqueeTextState createState() => _MarqueeTextState();
+}
+
+class _MarqueeTextState extends State<MarqueeText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),  
+      vsync: this,
+    )..repeat();
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: Row(
+        children: [
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: Offset(-MediaQuery.of(context).size.width * _animation.value, 0),
+                child: Row(
+                  children: [
+                    Text(widget.text, style: widget.style),
+                    SizedBox(width: 20),  
+                    Text(widget.text, style: widget.style),  
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
