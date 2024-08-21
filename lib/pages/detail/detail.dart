b28dc102df793d3fbd1e71dcc00674cfa754a5d9
Karol Stolarczyk <karolstolarczyk157@gmail.com>
2024-08-21 09:32:10 +0200

    101/fixes review

--- lib/pages/detail/detail.dart
+++ lib/pages/detail/detail.dart
@@ -4,7 +4,7 @@ import 'detail_lidl.dart';
 import 'detail_content.dart';
 
 class DetailPage extends StatelessWidget {
-  DetailPage({Key? key, required this.searchResult}) : super(key: key);
+  const DetailPage({Key? key, required this.searchResult}) : super(key: key);
 
   final SearchResult searchResult;
 
@@ -17,7 +17,7 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: _MarqueeText(searchResult.name ?? ""),
+        title: _ScrollingText(text: searchResult.name ?? "Unknown Company"),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
@@ -37,64 +37,85 @@ class DetailPage extends StatelessWidget {
   }
 }
 
-class _MarqueeText extends StatefulWidget {
+class _ScrollingText extends StatefulWidget {
   final String text;
 
-  const _MarqueeText(this.text, {Key? key}) : super(key: key);
+  const _ScrollingText({Key? key, required this.text}) : super(key: key);
 
   @override
-  State<_MarqueeText> createState() => _MarqueeTextState();
+  State<_ScrollingText> createState() => _ScrollingTextState();
 }
 
-class _MarqueeTextState extends State<_MarqueeText> with SingleTickerProviderStateMixin {
+class _ScrollingTextState extends State<_ScrollingText>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
-  static const int _maxCharacterCount = 20; 
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 10000),
+      duration: const Duration(seconds: 10), 
       vsync: this,
-    )..repeat();
+    )..repeat(); 
 
     _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
   }
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+    final screenWidth = MediaQuery.of(context).size.width;
+
+   
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: widget.text,
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      textDirection: TextDirection.ltr,
+      maxLines: 1,
+    )..layout();
+
+    final textWidth = textPainter.width;
 
     return Container(
       width: double.infinity,
-      child: ClipRect(
-        child: widget.text.length > _maxCharacterCount 
-          ? _buildAnimatedText(textStyle) 
-          : Text(widget.text, style: textStyle),
-      ),
-    );
-  }
+      height: 24,  
+      clipBehavior: Clip.hardEdge,
+      child: Row(
+        children: [
+    
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+ 
+              double offset = -screenWidth * _animation.value;
+ 
+              if (textWidth < screenWidth) {
+                offset = 0;
+              }
 
-  Widget _buildAnimatedText(TextStyle textStyle) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Row(
-          children: [
-            Transform.translate(
-              offset: Offset(-MediaQuery.of(context).size.width * _animation.value, 0),
-              child: Row(
-                children: [
-                  Text(widget.text, style: textStyle),
-                  const SizedBox(width: 20),  
-                  Text(widget.text, style: textStyle),  
-                ],
-              ),
-            ),
-          ],
-        );
-      },
+              return Transform.translate(
+              
+                offset: Offset(offset, 0),
+                child: Row(
+                  children: [
+                    Text(
+                      widget.text,
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(width: 20),  
+                    Text(
+                      widget.text,
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 
