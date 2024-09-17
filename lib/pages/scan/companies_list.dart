ed8e7ff3ec0bddb05e350ce58957cd3885d9e2e5
Adam Stolarczyk <adamstolarczyk2000@gmail.com>
2024-09-17 21:49:23 +0200

    fixes review

--- lib/pages/scan/companies_list.dart
+++ lib/pages/scan/companies_list.dart
@@ -13,39 +13,57 @@ class CompaniesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int listSize = state.list.length;
+
     _scrollToTop();
+
+    double maxHeight;
+    if (listSize == 1) {
+      maxHeight = 47.5;
+    } else if (listSize == 2) {
+      maxHeight = 95.0;
+    } else if (listSize == 3) {
+      maxHeight = 142.5;
+    } else {
+      maxHeight = 190.0;
+    }
+
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          _ListHeader(listSize: state.list.length),
-          Container(
-            height: 49.0 * state.list.length,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: ListView.builder(
-                controller: listScrollController,
-                reverse: true,
-                itemCount: state.list.length + (state.isLoading ? 1 : 0),
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == state.list.length) {
-                    return LoadingListItem();
-                  }
-                  return GestureDetector(
-                    child: ResultListItem(state.list[index]),
-                    onTap: () {
-                      final result = state.list[index];
-                      _analytics.opensCard(result);
-                      Navigator.pushNamed(context, '/detail',
-                          arguments: result);
-                    },
-                  );
-                },
-              ),
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        _ListHeader(listSize: listSize),
+        Container(
+          constraints: BoxConstraints(
+            maxHeight: maxHeight,
+          ),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView.builder(
+              controller: listScrollController,
+              reverse: true,
+              itemCount: listSize + (state.isLoading ? 1 : 0),
+              itemBuilder: (BuildContext context, int index) {
+                if (index == listSize) {
+                  return LoadingListItem();
+                }
+                return GestureDetector(
+                  child: ResultListItem(state.list[index]),
+                  onTap: () {
+                    final result = state.list[index];
+                    _analytics.opensCard(result);
+                    Navigator.pushNamed(context, '/detail', arguments: result);
+                  },
+                );
+              },
             ),
           ),
-          RemoteButton(RemoteButtonState(
-              state.list.firstOrNull?.donate, state.list.firstOrNull?.code))
-        ]);
+        ),
+        RemoteButton(RemoteButtonState(
+          state.list.firstOrNull?.donate,
+          state.list.firstOrNull?.code,
+        )),
+      ],
+    );
   }
 
   void _scrollToTop() {
