c218264a1d89f2857f611ef19896df05d9a50f8b
Karol Stolarczyk <karolstolarczyk157@gmail.com>
2024-09-17 19:06:28 +0200

    add no score message

---
+++ lib/pages/detail/no_score_message.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:pola_flutter/theme/assets.gen.dart';
+import 'package:pola_flutter/theme/colors.dart';
+import 'package:pola_flutter/theme/fonts.gen.dart';
+import 'package:pola_flutter/theme/text_size.dart';
+import 'package:pola_flutter/i18n/strings.g.dart';
+
+class NoScoreMessage extends StatelessWidget {
+  const NoScoreMessage({Key? key, required this.t}) : super(key: key);
+
+  final Translations t;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 22.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Assets.company.unpublished.svg(height: 109.42, width: 109.55),
+          ),
+          const SizedBox(height: 26.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 115.0),
+          ),
+          Text(
+            t.companyScreen.companyUnverified,
+            style: TextStyle(
+              fontSize: TextSize.description,
+              fontWeight: FontWeight.w400,
+              fontFamily: FontFamily.lato,
+              color: AppColors.text,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          Text(
+            t.companyScreen.thankYou,
+            style: TextStyle(
+              fontSize: TextSize.description,
+              fontWeight: FontWeight.w700,
+              fontFamily: FontFamily.lato,
+              color: AppColors.text,
+            ),
+            textAlign: TextAlign.left,
+          )
+        ],
+      ),
+    );
+  }
+}
