8bbfa0f6464394d747c4a039a0dec70db0debef3
Marcin Stepnowski <le0n.uo@gmail.com>
2024-10-08 16:40:29 +0200

    Add Row and Column

--- lib/pages/scan/scan_background.dart
+++ lib/pages/scan/scan_background.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:pola_flutter/theme/colors.dart';
 
 class ScanBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Positioned.fill(
-          child: Container(
-            color: AppColors.text.withOpacity(0.5),
-          ),
-      );
+     return 
+     Positioned.fill(
+          child:
+           Row(
+            children: [
+              _BlackOpacity(),
+              Column(
+                children: [
+                  _BlackOpacity(),
+                  _RedRectangle(),
+                  _BlackOpacity(),
+                ],
+              ),
+              _BlackOpacity(),
+            ]
+          )
+     );
+  }
+}
+
+class _BlackOpacity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: AppColors.text.withOpacity(0.9),
+      ),
+    );
+  }
+}
+
+class _RedRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+ return Container(
+      width: 250,
+      height: 187,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red, width: 2),
+        borderRadius: BorderRadius.circular(15), // Zaokrąglenie narożników
+      ),
+    );
   }
 }
